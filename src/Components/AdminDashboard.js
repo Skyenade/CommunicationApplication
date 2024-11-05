88ef46fb03dba7b4572aee96ec47d235e51970e7
MahammedNazim25 <148381801+MahammedNazim25@users.noreply.github.com>
2024-11-05 11:30:49 -0500

    AdminDashboardUpdate

--- src/Components/AdminDashboard.js
+++ src/Components/AdminDashboard.js
@@ -1,38 +1,53 @@
 import React from "react";
 import '../Style.css';
+import './AdminDashboard.css';
+import { useNavigate, Link } from "react-router-dom";
 import Header from "../Components/Header";
-import { useNavigate } from "react-router-dom";
+import HeaderAdmin from './HeaderAdmin';
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
 
   const handleCreateUser = () => {
-    navigate("/createUser"); // Create User page
+    navigate("/createUser"); 
   };
 
   const handleUserManagement = () => {
-    navigate("/userManagement"); //User Management page
+    navigate("/userManagement"); 
   };
 
   return (
-    <div className="adminDashboard">
-      <Header />
+    <div>
+    
+      <HeaderAdmin />
 
-      <aside className="sidebar">
-        <h2>EventsUp</h2>
-      </aside>
+      <div className="admin-dashboard">
+        <aside className="sidebar">
+          <h2>EventsUp</h2>
+        </aside>
 
-      <div className="content">
-        <h1>Admin Dashboard</h1>
+        <div className="content">
+          <h1>Admin Dashboard</h1>
 
-        <button className="createUserButton" onClick={handleCreateUser}>
-          Create an Account
-        </button>
+          <button className="createUserButton" onClick={handleCreateUser}>
+            Create an Account
+          </button>
 
-        <h2>User Management</h2>
-        <button onClick={handleUserManagement} className="manageUsersButton">
-          Manage Users
-        </button>
+          <h2>User Management</h2>
+          <button onClick={handleUserManagement} className="manageUsersButton">
+            Manage Users
+          </button>
+
+          <div className="dashboard-sections">
+            <div className="section">
+              <Link to="/UserManagement">User Management</Link>
+            </div>
+            <div className="section">Content Management</div>
+            <div className="section">Database Management</div>
+            <div className="section">History of Reported Users</div>
+            <div className="section">History of Reported Content</div>
+          </div>
+        </div>
       </div>
     </div>
   );
