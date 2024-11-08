197e5cec232574dbcbce0f32047db5b932904a1d
VivianaMv <VIVIANAMONTOYA14@GMAIL.COM>
2024-11-07 20:57:41 -0500

    Link User profilebtn to UserProfile page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -72,7 +72,7 @@ const Header = ({ handleSignOut, isSignedIn, userEmail }) => {
             </div>
 
             <div className="auth-buttons">
-                <button className="btn-user-profile">User Profile</button>
+                <button className="btn-user-profile" onClick={() => navigate("/UserProfile")}>User Profile</button>
                 <button onClick={handleSignOut} className="btnSignOut">Sign Out</button>
             </div>
         </div>
