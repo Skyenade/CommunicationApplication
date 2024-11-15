3f554ba74abc2841e8fbc48ec2e298801d98fd0d
Navneet Kamboj <148902434+navneet228@users.noreply.github.com>
2024-11-14 23:23:12 -0500

    update

--- src/Components/ModeratorHome.js
+++ src/Components/ModeratorHome.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Header from "./Header";
 import EventFeed from "./EventFeed";
 import { get, ref } from "firebase/database";
+import "../Style.css";
 
 const ModeratorHome = () => {
   const [notifications, setNotifications] = useState([]);
